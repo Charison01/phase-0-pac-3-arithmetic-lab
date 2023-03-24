7e512c02383a7e3cb2cf69ecd11bec72e198dfb3
Kelvin <kelvinkimaru@gmail.com>
2023-03-25 01:15:47 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+//Addition function
+function add() {}
+//Subtraction
+function subtract (){}
+//Divide
+function divide(){}
+//Multiple
+function multiply(){}
+//Add and return result
+function add(a,b){
+    return a + b;
+}
+//Subtract and return result
+function subtract(a,b){
+    return a - b;
+}
+//Multiply a and b
+function multiply(a,b){
+    return a*b;
+}
+//Divide a and b
+function divide(a,b){
+    return a/b;
+}
+//Increment
+function increment(n){
+    return ++n;
+}
+//Decrement
+function decrement(n){
+    return --n;
+}
+//Parsing data
+//Parse int
+function makeInt(n){
+    return parseInt(n, 10)
+}
+//Preserve decimals
+function preserveDecimal(n){
+    return parseFloat(n);
+}
